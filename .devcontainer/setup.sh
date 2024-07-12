352234ccf8d41e7ac3da0b031065b4b0f75a0ed2
Lakshmikanth <k.lakshmikanth002@gmail.com>
2024-07-12 19:52:41 +0530

    Create setup.sh

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,3 @@
+ollama pull llama3
+ollama pull mxbai-embed-large
+pip install -q llama-index llama-index-llms-ollama llama-index-vector-stores-qdrant llama-index-readers-file llama-index-embeddings-ollama qdrant-client
